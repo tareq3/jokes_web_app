f42a20bdc26c0960919b2ec5e680d3dbb498a50a
tareq3 <mti.tareq3@gmai.com>
2019-02-24 20:28:31 +0600

    xml config added

--- src/main/java/com/mti/jokes/JokesApplication.java
+++ src/main/java/com/mti/jokes/JokesApplication.java
@@ -2,8 +2,10 @@ package com.mti.jokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck_config.xml") //adding config
 public class JokesApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/mti/jokes/config/ChuckConfiguration.java
+++ src/main/java/com/mti/jokes/config/ChuckConfiguration.java
@@ -5,11 +5,11 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
     /*This is how we create bean*/
-    @Bean
+  //  @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
